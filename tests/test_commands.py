98f1e13e0addfe38dd358fab10a84bf05b19e521
cfc603 <wtrevor162@gmail.com>
2017-10-14 09:38:43 -0400

    Command takes optional api_key & user for PythonAnywhere instance. Continues to look for environment vars if not passed as args

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,10 +1,41 @@
+import os
+
 from mock import patch
 
 from pythonanywhere.client import PythonAnywhereError
 from pythonanywhere_cli.commands import Command, StaticFile, Webapps
 
 
-class TestCommand(object):
+class CommonTestFunctions(object):
+
+    def command_options(self, **kwargs):
+        options = {
+            "--api_key": None,
+            "--help": False,
+            "--path": None,
+            "--python_version": None,
+            "--url": None,
+            "--user": None,
+            "--version": False,
+            "--virtualenv_path": None,
+            "<domain_name>": None,
+            "<path>": None,
+            "<python_version>": None,
+            "<static_id>": None,
+            "<url>": None,
+            "create": False,
+            "delete": False,
+            "list": True,
+            "reload": False,
+            "static_mapping": True,
+            "update": False,
+            "webapps": False
+        }
+        options.update(kwargs)
+        return options
+
+
+class TestCommand(CommonTestFunctions):
 
     class CommandException(Command):
 
@@ -15,20 +46,40 @@ class TestCommand(object):
         def exception(self):
             raise PythonAnywhereError("Test Exception")
 
+    def test_get_client_if_options(self):
+        command = Command(self.command_options(**{
+                "--api_key": "Test Key", "--user": "Test User"
+            }))
+
+        assert command.client.api_key == "Test Key"
+        assert command.client.user == "Test User"
+
+    def test_get_client_if_not_options(self):
+        os.environ["PYTHONANYWHERE_CLI_API_KEY"] = "Test Key"
+        os.environ["PYTHONANYWHERE_CLI_USER"] = "Test User"
+        command = Command(self.command_options())
+
+        assert command.client.api_key == "Test Key"
+        assert command.client.user == "Test User"
+
     @patch("pythonanywhere_cli.commands.snakesay")
     def test_run_exception(self, snakesay):
-        command_exception = self.CommandException({"exception": True})
+        command_exception = self.CommandException(
+            self.command_options(**{"exception": True})
+        )
         command_exception.run()
         snakesay.assert_called_with("Test Exception")
 
 
-class TestStaticFile(object):
+class TestStaticFile(CommonTestFunctions):
 
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_create(self, mock_client, snakesay):
         static_file = StaticFile({
                 "create": True,
+                "--api_key": None,
+                "--user": None,
                 "<domain_name>": "tests.domain.com",
                 "<url>": "/url/",
                 "<path>": "/path/"
@@ -37,8 +88,8 @@ class TestStaticFile(object):
         client_call = static_file.client.webapps.static_files.create
 
         client_call.assert_called_with(
-            data={'url': '/url/', 'path': '/path/'},
-            domain_name='tests.domain.com'
+            data={"url": "/url/", "path": "/path/"},
+            domain_name="tests.domain.com"
         )
         snakesay.assert_called_with((
             "Static File mapping created for domain"
@@ -50,6 +101,8 @@ class TestStaticFile(object):
     def test_delete(self, mock_client, snakesay):
         static_file = StaticFile({
                 "delete": True,
+                "--api_key": None,
+                "--user": None,
                 "<domain_name>": "tests.domain.com",
                 "<static_id>": "123"
             })
@@ -67,13 +120,13 @@ class TestStaticFile(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_list(self, mock_client, snakesay):
-        static_file = StaticFile({
+        static_file = StaticFile(self.command_options(**{
                 "list": True,
                 "<domain_name>": "tests.domain.com",
-            })
+            }))
         client_call = static_file.client.webapps.static_files
         client_call.return_value.json.return_value = [
-            {u'url': u'/static/', u'path': u'/static/', u'id': 123},
+            {u"url": u"/static/", u"path": u"/static/", u"id": 123},
         ]
         static_file.run()
 
@@ -87,13 +140,13 @@ class TestStaticFile(object):
 
     @patch("pythonanywhere_cli.commands.snakesay")
     def test_update_if_not_url_if_not_path(self, snakesay):
-        static_file = StaticFile({
+        static_file = StaticFile(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "<static_id>": "123",
                 "--url": None,
                 "--path": None,
-            })
+            }))
         static_file.run()
 
         snakesay.assert_called_with((
@@ -104,13 +157,13 @@ class TestStaticFile(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_update_if_url(self, mock_client, snakesay):
-        static_file = StaticFile({
+        static_file = StaticFile(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "<static_id>": "123",
                 "--url": "/url/",
                 "--path": None,
-            })
+            }))
         static_file.run()
         client_call = static_file.client.webapps.static_files.update
 
@@ -127,13 +180,13 @@ class TestStaticFile(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_update_if_path(self, mock_client, snakesay):
-        static_file = StaticFile({
+        static_file = StaticFile(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "<static_id>": "123",
                 "--url": None,
                 "--path": "/path/",
-            })
+            }))
         static_file.run()
         client_call = static_file.client.webapps.static_files.update
 
@@ -148,16 +201,16 @@ class TestStaticFile(object):
         ))
 
 
-class TestWebapps(object):
+class TestWebapps(CommonTestFunctions):
 
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_create(self, mock_client, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "create": True,
                 "<domain_name>": "tests.domain.com",
                 "<python_version>": "python27",
-            })
+            }))
         webapp.run()
         client_call = webapp.client.webapps.create
 
@@ -174,10 +227,10 @@ class TestWebapps(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_delete(self, mock_client, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "delete": True,
                 "<domain_name>": "tests.domain.com",
-            })
+            }))
         webapp.run()
         client_call = webapp.client.webapps.delete
 
@@ -190,10 +243,10 @@ class TestWebapps(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_reload(self, mock_client, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "reload": True,
                 "<domain_name>": "tests.domain.com",
-            })
+            }))
         webapp.run()
         client_call = webapp.client.webapps.reload
 
@@ -205,12 +258,12 @@ class TestWebapps(object):
 
     @patch("pythonanywhere_cli.commands.snakesay")
     def test_update_if_not_virtualenv_path_if_not_python_vers(self, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "--virtualenv_path": None,
                 "--python_version": None,
-            })
+            }))
         webapp.run()
 
         snakesay.assert_called_with((
@@ -221,12 +274,12 @@ class TestWebapps(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_update_if_virtualenv_path(self, mock_client, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "--virtualenv_path": "/path/",
                 "--python_version": None,
-            })
+            }))
         webapp.run()
         client_call = webapp.client.webapps.update
 
@@ -241,12 +294,12 @@ class TestWebapps(object):
     @patch("pythonanywhere_cli.commands.snakesay")
     @patch("pythonanywhere_cli.commands.PythonAnywhere")
     def test_update_if_python_version(self, mock_client, snakesay):
-        webapp = Webapps({
+        webapp = Webapps(self.command_options(**{
                 "update": True,
                 "<domain_name>": "tests.domain.com",
                 "--virtualenv_path": None,
                 "--python_version": "python27",
-            })
+            }))
         webapp.run()
         client_call = webapp.client.webapps.update
 
